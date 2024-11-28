c5c9a68dc328aa26a8661c3f32eca182bad91b08
Shunsuke-Banjo <shunsuke.banjo@link-u.co.jp>
2024-11-28 11:14:52 +0900

    feat:

--- app/src/main/java/com/example/coderabbit_test/MainActivity.kt
+++ app/src/main/java/com/example/coderabbit_test/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("build-check-test")
+                    Greeting("build-check-test2")
                 }
             }
         }
